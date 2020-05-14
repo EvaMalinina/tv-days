878b72443a4ae79e076b39f758d9172d8a3c054b
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-05-14 19:17:08 +0300

    travis setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/tv-days'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/tv-days/index.html'));
+});
+
+app.listen(process.env.PORT || 8081);
