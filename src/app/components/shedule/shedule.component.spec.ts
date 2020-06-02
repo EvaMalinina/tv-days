5b156c7665a13f04e9208505a3c532e5b8361ec3
Tatyana Dotsenko <tetiana_dotsenko@epam.com>
2020-06-02 13:23:04 +0300

    + RouterTestingModule in shows-info & shedule

--- src/app/components/shedule/shedule.component.spec.ts
+++ src/app/components/shedule/shedule.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SheduleComponent } from './shedule.component';
 import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('SheduleComponent', () => {
   let component: SheduleComponent;
@@ -8,7 +9,7 @@ describe('SheduleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule ],
+      imports: [ HttpClientModule, RouterTestingModule ],
       declarations: [ SheduleComponent ]
     })
     .compileComponents();
--- src/app/components/show-info/show-info.component.spec.ts
+++ src/app/components/show-info/show-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { ShowInfoComponent } from './show-info.component';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('ShowInfoComponent', () => {
   let component: ShowInfoComponent;
@@ -8,7 +9,7 @@ describe('ShowInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule ],
+      imports: [ HttpClientModule, RouterTestingModule ],
       declarations: [ ShowInfoComponent ]
     })
     .compileComponents();
